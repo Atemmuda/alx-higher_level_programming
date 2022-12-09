b33ea2b1ce111b3f8b6dff1c6d782006e96ebffa
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-12-08 20:16:08 -0800

    fixes to file 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    result_matrix = [] * len(matrix)
-    row_index = 0;
-    for row in matrix:
-        result_matrix[row_index] = [x * x for x in row]
-        row_idex += 1
-    return(result_matrix)
+    return([item**2 for item in row]for row in matrix)
