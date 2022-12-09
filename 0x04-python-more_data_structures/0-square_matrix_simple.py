d4ad7f8aeab5566f56c5a351b76f4b0f995173b6
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-12-08 20:06:59 -0800

    coreected file 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return ([list(map(lambda x: x**2, row))f for row in matrix])
+    result_matrix = [[1]] * len(matrix)
+    row_index = 0;
+    for row in matrix:
+        result_matrix[row_index] = [x * x for x in row]
+        row_idex += 1
+    return (result_matrix)
