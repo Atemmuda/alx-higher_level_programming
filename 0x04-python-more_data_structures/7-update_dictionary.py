3c832318bb6bfad297385374677a1aa3bc2f35b8
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-12-09 08:16:38 -0800

    Correted file 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -7,6 +7,7 @@ def update_dictionary(a_dictionary, key, value):
     for k, v in a_dictionary.items():
         if k == key:
             a_dictionary[k] = value
+            break;
         else:
             a_dictionary[key] = value
     return (a_dictionary)
