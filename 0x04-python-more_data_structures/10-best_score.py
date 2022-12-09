6ad726f3a095f78e3fc133216aa185ab39ae2582
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-12-09 15:30:44 -0800

    corrected file 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     best_score = 0
-    key = ""
+    if not a_dictionary:
+        return None
     for key, v in a_dictionary.items():
         if v > best_score:
             best_score = v
-    return (key)
+        return (key)
