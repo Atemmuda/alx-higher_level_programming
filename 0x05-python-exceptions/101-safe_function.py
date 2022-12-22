ae1f0b78bfb39b2a3765b8a15e881b6005c05028
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-12-22 12:07:54 -0800

    Added file 101 correction

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,7 +2,8 @@
 def safe_function(fct, *args):
     import sys
     try:
-        return (fct(args))
+        result = fct(args)
     except Exception as error:
         sys.stderr.write("Exception: {}\n".format(error))
-        return (None)
+        result = None
+    return result
