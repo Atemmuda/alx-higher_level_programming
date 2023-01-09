f080758c91b6d483e32326bd870c593e8f2b2ebb
Atemmuda <yaw.ayeboahfo@gmail.com>
2023-01-09 03:22:14 -0800

    Fixes to file 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,16 +1,34 @@
-#!/usr/bin/python3
 """Module containing function to print string
 by line. It contains no directly executable code
 It should be imported as a module
 """
 
 def text_indentation(text):
-    if type(text) is not str:
-        raise TypeError("text must be string")
-    delimeters = '?.:'
-    for c in delimeters:
-        text = str(c +'\n\n'.join(s.strip() for s in text.split(c)))
-    print(text, end='')
-    if len(text) > 0 and text[-1] in delimeters:
-        print('\n')
+    """Print a given text by indenting
+    when there is one of a given delimeters
 
+    Args:
+        text (string): parameter to process
+
+    Raises:
+        TypeError: if text is other than string
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    previous = ""
+    for ch in text:
+        # leading whitesapces
+        if ch is " " and ch is text[0] and previous is "":
+            previous = "\n"
+            continue
+        # whitespace after newline
+        if ch is " " and previous is "\n":
+            continue
+        # getting the delimeter, print it and new anewline
+        if ch is "." or ch is "?" or ch is ":":
+            print(ch)
+            print()
+            previous = "\n"
+        else:
+            print(ch, end="")
+            previous = ch
