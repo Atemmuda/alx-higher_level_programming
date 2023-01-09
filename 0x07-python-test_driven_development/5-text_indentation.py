e938063447a7e69916fcac94623644288c320108
Atemmuda <yaw.ayeboahfo@gmail.com>
2023-01-09 02:46:28 -0800

    Added file 1

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,15 @@
+"""Module containing function to print string
+by line. It contains no directly executable code
+It should be imported as a module
+"""
+
+def text_indentation(text):
+    if type(text) is not str:
+        raise TypeError("text must be string")
+    delimeters = '?.:'
+    for c in delimeters:
+        text = str(c +'\n\n'.join(s.strip() for s in text.split(c)))
+    print(text, end='')
+    if len(text) > 0 and text[-1] in delimeters:
+        print('\n')
+
